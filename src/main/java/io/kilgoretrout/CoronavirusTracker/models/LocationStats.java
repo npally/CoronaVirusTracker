e78de4b3e48673d0e9fe57e234c8dde6e619af6b
Kilgore Trout <nicholaspally@gmail.com>
2020-03-01 18:57:03 -0800

    initial commit

---
+++ src/main/java/io/kilgoretrout/CoronavirusTracker/models/LocationStats.java
@@ -0,0 +1,49 @@
+package io.kilgoretrout.CoronavirusTracker.models;
+
+public class LocationStats {
+    private String state;
+    private String country;
+    private int latestTotalCases;
+    private int diffFromYesterday;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getLatestTotalCases() {
+        return latestTotalCases;
+    }
+
+    public void setLatestTotalCases(int latestTotalCases) {
+        this.latestTotalCases = latestTotalCases;
+    }
+
+    public int getDiffFromYesterday() {
+        return diffFromYesterday;
+    }
+
+    public void setDiffFromYesterday(int diffFromYesterday) {
+        this.diffFromYesterday = diffFromYesterday;
+    }
+
+    @Override
+    public String toString() {
+        return "LocationStats{" +
+                "state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", latestTotalCases=" + latestTotalCases +
+                '}';
+    }
+}
